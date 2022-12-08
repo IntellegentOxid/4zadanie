07a0deb8c944ab2c839c88bef86e81170759e31a
revelations? <flfkflfkkf@gmail.com>
2022-12-08 11:34:13 +0200

    Добавьте файлы проекта.

---
+++ 4zad/4zad.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, ".1251");
+	double x, n;
+	x = 3;
+	cout << "Введіть n=";
+	cin >> n;
+	double y, p, t, e;
+	e = 2,718281828459;
+	t = x * x + sqrt(abs(n));
+	p = e * sqrt(t);
+	y = pow(log(pow(p + t, 2)), 2);
+	cout << "Результат y=" << y << endl;
+	system("pause>>void");
+	return 0;
+}
